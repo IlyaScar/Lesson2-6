10849ca0b96ae7ec4f3d8896ebd2c2d957dad804
Markin Pavel <markinpashka@gmail.com>
2026-02-12 08:18:21 +0300

    Comments lesson 2_6

--- src/Main.kt
+++ src/Main.kt
@@ -12,7 +12,8 @@ fun main() {
       println(min)
       val averagePrice = prices.average()
       println(averagePrice)
-      for (prices in prices) {
+    //Тут надо испольозвать функции filter и foreach
+    for (prices in prices) {
           if (prices > 50) {
 
               println(prices)
@@ -22,12 +23,14 @@ fun main() {
 
      //task 2
       val users = listOf("alice", "bob", "charlie", "david", "eve")
-          val upper = users[0].uppercase()
+        //Вид к которому ты приводшь первое имя отличается от того что в задании, + приводить надо все имена, а не первое  
+        val upper = users[0].uppercase()
           println(upper)
+          //Узнали про циклы и забываем их использовать в коллекциях,там все готовые методы есть из коробки, про которые я рассказывал, тут нужно использовать find
           for (users in users) {
               if (users.length > 4) {
 
-                  println(users[0])
+                  println(users[0]) //А еще даже так это не будет работать, ты будешь всегда выводить первое имя когда найдешь какое-то больше 4 символов
               }
 
           }
@@ -47,6 +50,7 @@ fun main() {
 
    // val results = apiResponse.values
    // println(results)
+   //Вывод проверял тут? У меняне выводит ни количество активных пользователей, не их самих
     val results2 = apiResponse.filterValues { apiResponse.contains("active") }
     println(results2)
 
