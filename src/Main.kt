8a09d82356ed9a236ccef89e6915c451b7a353c6
Ilya Smolin <ilyasmolin9@gmail.com>
2026-03-10 12:09:57 +0300

    Fix

--- src/Main.kt
+++ src/Main.kt
@@ -6,36 +6,25 @@ fun main() {
 
     //task 1
     val prices = listOf(99.99,25.50,75.00)
-      val max = prices.min()
-      println(max)
-      val min = prices.max()
-      println(min)
-      val averagePrice = prices.average()
-      println(averagePrice)
+    val max = prices.min()
+    println(max)
+    val min = prices.max()
+    println(min)
+    val averagePrice = prices.average()
+    println(averagePrice)
     //Тут надо испольозвать функции filter и foreach
-    for (prices in prices) {
-          if (prices > 50) {
-
-              println(prices)
-          }
-
-      }
+    //а зачем for each?
+    val moreFifty = prices.filter { it > 50 }
+    println(moreFifty)
 
      //task 2
       val users = listOf("alice", "bob", "charlie", "david", "eve")
-        //Вид к которому ты приводшь первое имя отличается от того что в задании, + приводить надо все имена, а не первое  
-        val upper = users[0].uppercase()
-          println(upper)
+        //Вид к которому ты приводшь первое имя отличается от того что в задании, + приводить надо все имена, а не первое
+        val upper = users.map { (it.uppercase())
+        }
           //Узнали про циклы и забываем их использовать в коллекциях,там все готовые методы есть из коробки, про которые я рассказывал, тут нужно использовать find
-          for (users in users) {
-              if (users.length > 4) {
-
-                  println(users[0]) //А еще даже так это не будет работать, ты будешь всегда выводить первое имя когда найдешь какое-то больше 4 символов
-              }
-
-          }
-          val sort = users.sortedBy { it.length }
-          println(sort)
+         val longName = users.find { it.length > 4 }
+         val groupByName = users.groupBy { it.length }
 
     //task 3
     val apiResponse = mapOf(
@@ -43,21 +32,16 @@ fun main() {
             mapOf("id" to 1, "status" to "active"),
             mapOf("id" to 2, "status" to "inactive"),
             mapOf("id" to 3, "status" to "active")
-
         ),
         "total" to 3
     )
 
-   // val results = apiResponse.values
-   // println(results)
    //Вывод проверял тут? У меняне выводит ни количество активных пользователей, не их самих
-    val results2 = apiResponse.filterValues { apiResponse.contains("active") }
-    println(results2)
-
-
-
-
-
-
-
+    //поправил, но не могу понять у меня синтаксиси не правильный или я что то не так делаю
+    // я не прошу тут ответ расписывать, просто небольшую подсказку или голосом можно если долго писать
+    val results = apiResponse.values
+    val activeUsers = results.filter{ it["status"] == "active" }
+    val countActive = activeUsers.size
+    println(activeUsers)
+    println(countActive)
 }
